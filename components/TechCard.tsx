2018fe781c59cea6f9fe8900130e8c9cf747a523
Dima <linklib.web@yandex.ru>
2021-11-02 09:26:54 +0300

    Add bootstrap

--- components/TechCard.tsx
+++ components/TechCard.tsx
@@ -1,27 +1,37 @@
 import { Tech } from "@prisma/client";
 import Image from "next/image";
+import { Col } from "react-bootstrap";
 
 interface TechCardProps {
-  tech: Tech;
+  tech: any;
+  //tech: Tech;
 }
 
 export default function TechCard(props: TechCardProps) {
   return (
-    <div className="border rounded-lg p-4 flex">
-      <div className="my-auto">
+    <>
+      <Col xs={12} sm={12} md={3}>
         <Image
           src={props.tech.image ? props.tech.image : "no-image.jpg"}
           alt="Logo"
-          width={100}
-          height={100}
+          width={150}
+          height={150}
         />
-      </div>
-      <div className="ml-4">
-        <p className="text-xl text-gray-700">
-          {props.tech.name} {props.tech.desc}
-        </p>
-        <p className="text-gray-500">{props.tech.categoryId}</p>
-      </div>
-    </div>
+      </Col>
+      <Col xs={12} sm={12} md={3}>
+        <h3>{props.tech.name}</h3>
+        <p>{props.tech.desc}</p>
+      </Col>
+      <Col xs={12} sm={12} md={3}>
+        Уровень:
+        <br />
+        <strong>{props.tech.lavel}</strong>
+      </Col>
+      <Col xs={12} sm={12} md={3}>
+        Категория:
+        <br />
+        <strong>{props.tech.Category.name}</strong>
+      </Col>
+    </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
 import { NextSeo } from "next-seo";
 import { useState } from "react";
 import TechCard from "./../components/TechCard";
+import { Container, Row } from "react-bootstrap";
 
 import { PrismaClient, Tech, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export async function getServerSideProps() {
-  const teches: Tech[] = await prisma.tech.findMany();
+  const teches: Tech[] = await prisma.tech.findMany({
+    include: {
+      Category: true,
+    },
+  });
   return {
     props: {
       initialTeches: teches,
@@ -17,19 +22,21 @@ export async function getServerSideProps() {
 
 export const Home = ({ initialTeches }: { initialTeches: any }) => {
   const [teches, setTeches] = useState<Tech[]>(initialTeches);
-  //console.log("teches", teches);
+  console.log("teches", teches);
   return (
     <>
-      <NextSeo title="Main page" description="Main page description" />
+      <NextSeo
+        title="Резюме Попова Дмитрия"
+        description="Резюме Попова Дмитрия: junior developer"
+      />
 
-      <div>Main Page</div>
-
-      {teches.map((t, i: number) => (
-        <div className="mb-3" key={i}>
-          <TechCard tech={t} />
-          {/*<div>{c.name}</div>*/}
-        </div>
-      ))}
+      <Container>
+        {teches.map((t, i: number) => (
+          <Row key={i} className="border-start border-info mt-3 p-2 shadow">
+            <TechCard tech={t} />
+          </Row>
+        ))}
+      </Container>
     </>
   );
 };
